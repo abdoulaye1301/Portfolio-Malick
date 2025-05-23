3f078a0f05e93f4852fef332c0b50c0bc12fb910
Abdoulaye NDAO <ndao1301@gmail.com>
2025-05-23 17:59:36 +0000

    Modification de ID GLOBAL

--- views/Projets.py
+++ views/Projets.py
@@ -148,8 +148,6 @@ col[1].write(
     une vue globale sur la progression des enquetes au près des ménages pour mener à bien la
     réalisation du projet d'enquete dans le cadre de la mise à jours et extention
     du Registre National Unique (RNU).
-    
-    <p style="text-align: justify;">
     Ce travail consiste à mettre en place un tableau de bord qui va nous permetre de suivre en temps réel 
     
     </B></p>
@@ -166,11 +164,12 @@ col[0].image("static/Departement de Kaffine.jpg")
 # """,
 # unsafe_allow_html=True,
 # )
-for i in range(4):
+for i in range(1):
     col[1].write("\n")
 
 # ===============Développement d'application===========================
-
+for i in range(3):
+    col[0].write("\n")
 col[0].write(
     """<h2 align=right><FONT color="orange">Développement </FONT></h2>""",
     unsafe_allow_html=True,
